faa0abffae99fe782d52dd648b57adc8753d113d
Jim Newpower <jimnewpower@gmail.com>
2021-02-03 12:40:05 -0700

    Reformat

--- src/main/java/com/primalimited/reliefshading/bounds/NullBounds.java
+++ src/main/java/com/primalimited/reliefshading/bounds/NullBounds.java
@@ -3,32 +3,32 @@ package com.primalimited.reliefshading.bounds;
 import com.primalimited.reliefshading.number.Invalid;
 
 final class NullBounds implements Bounds {
-  public static Bounds create() {
-    return new NullBounds();
-  }
-  
-  @Override
-  public String toString() {
-    return getClass().getSimpleName() + " " + text(); 
-  }
+    public static Bounds create() {
+        return new NullBounds();
+    }
 
-  @Override
-  public double min() {
-    return Invalid.INVALID_DOUBLE;
-  }
+    @Override
+    public String toString() {
+        return getClass().getSimpleName() + " " + text();
+    }
 
-  @Override
-  public double max() {
-    return Invalid.INVALID_DOUBLE;
-  }
+    @Override
+    public double min() {
+        return Invalid.INVALID_DOUBLE;
+    }
 
-  @Override
-  public double range() {
-    return Invalid.INVALID_DOUBLE;
-  }
+    @Override
+    public double max() {
+        return Invalid.INVALID_DOUBLE;
+    }
 
-  @Override
-  public boolean isValid() {
-    return false;
-  }
+    @Override
+    public double range() {
+        return Invalid.INVALID_DOUBLE;
+    }
+
+    @Override
+    public boolean isValid() {
+        return false;
+    }
 }
