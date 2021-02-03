92e7fb13de5a67cb3bf54d369440caaba56297bf
Jim Newpower <jimnewpower@gmail.com>
2021-02-03 12:40:26 -0700

    Reformat

--- src/main/java/com/primalimited/reliefshading/bounds/EmptyBounds.java
+++ src/main/java/com/primalimited/reliefshading/bounds/EmptyBounds.java
@@ -3,38 +3,38 @@ package com.primalimited.reliefshading.bounds;
 import com.primalimited.reliefshading.number.Invalid;
 
 final class EmptyBounds implements Bounds {
-  static final double DEFAULT_VALUE = Invalid.INVALID_DOUBLE;
+    static final double DEFAULT_VALUE = Invalid.INVALID_DOUBLE;
 
-  static EmptyBounds create() {
-    return new EmptyBounds();
-  }
+    static EmptyBounds create() {
+        return new EmptyBounds();
+    }
 
-  private EmptyBounds() {
-  }
+    private EmptyBounds() {
+    }
 
-  @Override
-  public String toString() {
-    String text = "[null..null]";
-    return getClass().getSimpleName() + " " + text;
-  }
+    @Override
+    public String toString() {
+        String text = "[null..null]";
+        return getClass().getSimpleName() + " " + text;
+    }
 
-  @Override
-  public double min() {
-    return DEFAULT_VALUE;
-  }
+    @Override
+    public double min() {
+        return DEFAULT_VALUE;
+    }
 
-  @Override
-  public double max() {
-    return DEFAULT_VALUE;
-  }
+    @Override
+    public double max() {
+        return DEFAULT_VALUE;
+    }
 
-  @Override
-  public double range() {
-    return Double.MAX_VALUE;
-  }
+    @Override
+    public double range() {
+        return Double.MAX_VALUE;
+    }
 
-  @Override
-  public boolean isValid() {
-    return false;
-  }
+    @Override
+    public boolean isValid() {
+        return false;
+    }
 }
