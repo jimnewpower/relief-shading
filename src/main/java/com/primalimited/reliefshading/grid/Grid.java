954038258d334d09f61cc3b9d1c085bd47bb6a53
Jim Newpower <jimnewpower@gmail.com>
2021-02-03 16:20:28 -0700

    Add javadoc

--- src/main/java/com/primalimited/reliefshading/grid/Grid.java
+++ src/main/java/com/primalimited/reliefshading/grid/Grid.java
@@ -3,7 +3,21 @@ package com.primalimited.reliefshading.grid;
 import com.primalimited.reliefshading.bounds.Bounds2D;
 
 public interface Grid {
+    /**
+     * Return number of rows in the grid.
+     * @return number of rows in the grid.
+     */
     int rows();
+
+    /**
+     * Return number of columns in the grid.
+     * @return number of columns in the grid.
+     */
     int columns();
+
+    /**
+     * Return the spatial extents of the grid.
+     * @return the spatial extents of the grid.
+     */
     Bounds2D bounds();
 }
