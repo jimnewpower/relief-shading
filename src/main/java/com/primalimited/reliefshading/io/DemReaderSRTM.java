d66c1eb77131475d0f1bb70e27ac41851b38a257
Jim Newpower <jimnewpower@gmail.com>
2021-02-09 15:39:47 -0700

    Implement reader for SRTM .hgt DEM files

--- src/main/java/com/primalimited/reliefshading/io/DemReaderSRTM.java
+++ src/main/java/com/primalimited/reliefshading/io/DemReaderSRTM.java
@@ -1,11 +1,24 @@
 package com.primalimited.reliefshading.io;
 
+import com.primalimited.reliefshading.bounds.Bounds;
+import com.primalimited.reliefshading.bounds.Bounds2D;
 import com.primalimited.reliefshading.grid.Grid;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
+import java.nio.channels.FileChannel;
 import java.nio.file.Path;
 import java.util.Objects;
 
-public class DemReaderSRTM implements DemReader {
+class DemReaderSRTM implements DemReader {
+    private static final int N_ROWS = 3601;
+    private static final int N_COLS = 3601;
+    private static final int SIZE = N_ROWS * N_COLS;
+    private static final ByteOrder BYTE_ORDER = ByteOrder.BIG_ENDIAN;
+
     private transient final Path path;
 
     DemReaderSRTM(Path path) {
@@ -14,6 +27,98 @@ public class DemReaderSRTM implements DemReader {
 
     @Override
     public Grid read() {
-        return null;
+        Bounds2D bounds = createBounds();
+        Short[] values = new Short[SIZE];
+
+        try (FileChannel fileChannel = new FileInputStream(path.toFile()).getChannel()) {
+            fileChannel.position(0L);
+
+            ByteBuffer byteBuffer = allocate();
+
+            fileChannel.read(byteBuffer, 0L);
+
+            for (int index = 0; index < SIZE; index++)
+                values[index] = byteBuffer.getShort(index * Short.BYTES);
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
+        return Grid.createRowMajorSWOrigin(N_ROWS, N_COLS, bounds, values);
+    }
+
+    private ByteBuffer allocate() {
+        ByteBuffer byteBuffer = ByteBuffer.allocate(nBytes());
+        byteBuffer.order(BYTE_ORDER);
+        return byteBuffer;
+    }
+
+    private int nBytes() {
+        return Short.BYTES * SIZE;
+    }
+
+    Bounds2D createBounds() {
+        return Bounds2D.create(getLongitudeBounds(), getLatitudeBounds());
+    }
+
+    Bounds getLongitudeBounds() {
+        // Filename longitude is for west edge of quad, so add 1 for max
+        int longitude = getLongitude();
+        return Bounds.of(longitude, longitude + 1.0);
+    }
+
+    Bounds getLatitudeBounds() {
+        // Filename latitude is for south edge of quad, so add 1 for max
+        int latitude = getLatitude();
+        return Bounds.of(latitude, latitude + 1.0);
+    }
+
+    int getLongitude() {
+        String[] split = filename().split(getLatitudeText());
+        split = split[1].split(getLongitudeText());
+        String str = split[1].substring(0, split[1].indexOf("."));
+        int longitude = Integer.parseInt(str);
+        if (isWest())
+            longitude = -longitude;
+        return longitude;
+    }
+
+    int getLatitude() {
+        String str = filename().split(getLatitudeText())[1].split(getLongitudeText())[0];
+        int latitude = Integer.parseInt(str);
+        if (isSouth())
+            latitude = -latitude;
+        return latitude;
+    }
+
+    String getLatitudeText() {
+        return isNorth() ? "N" : "S";
+    }
+
+    String getLongitudeText() {
+        return isWest() ? "W" : "E";
+    }
+
+    boolean isNorth() {
+        return filename().contains("N");
+    }
+
+    boolean isSouth() {
+        return filename().contains("S");
+    }
+
+    boolean isEast() {
+        return filename().contains("E");
+    }
+
+    boolean isWest() {
+        return filename().contains("W");
+    }
+
+    String filename() {
+        return path.getFileName().toString().toUpperCase();
     }
 }
--- src/test/java/com/primalimited/reliefshading/io/DemReaderTest.java
+++ src/test/java/com/primalimited/reliefshading/io/DemReaderTest.java
@@ -1,5 +1,6 @@
 package com.primalimited.reliefshading.io;
 
+import com.primalimited.reliefshading.bounds.Bounds2D;
 import com.primalimited.reliefshading.grid.Grid;
 import org.junit.jupiter.api.Test;
 
@@ -7,16 +8,39 @@ import java.net.URL;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
-import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class DemReaderTest {
+    private static final String DEMO_FILENAME = "N37W108.hgt";
+
     @Test
     public void readSRTM_HGT_DEM() {
-        URL url = getClass().getResource("N37W108.hgt");
+        URL url = getClass().getResource(DEMO_FILENAME);
         assertNotNull(url, "url");
 
-        Path path = Paths.get("");
+        Path path = Path.of(url.getPath());
+        assertNotNull(path, "Path");
+
         DemReader reader = DemReader.shuttleRadarTopographyMissionHGT(path);
         Grid grid = reader.read();
+        assertNotNull(grid, "Grid");
+
+        final double tolerance = 1e-8;
+        Bounds2D bounds = grid.bounds();
+        assertAll("Bounds checks for " + DEMO_FILENAME,
+                () -> assertEquals(-108.0, bounds.minX(), tolerance),
+                () -> assertEquals(-107.0, bounds.maxX(), tolerance),
+                () -> assertEquals(37.0, bounds.minY(), tolerance),
+                () -> assertEquals(38.0, bounds.maxY(), tolerance)
+        );
+
+        short expected = 2528;
+        assertEquals(expected, grid.value(0, 0));
+        expected = 2000;
+        assertEquals(expected, grid.value(3600, 0));
+        expected = 2535;
+        assertEquals(expected, grid.value(3600, 3600));
+        expected = 3776;
+        assertEquals(expected, grid.value(0, 3600));
     }
 }
