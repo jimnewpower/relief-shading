d4e12530ca7b3e68c2d37ffedf88220c536a69b4
Jim Newpower <jimnewpower@gmail.com>
2021-02-09 12:00:25 -0700

    Fix PMD warnings, bolster coverage

--- src/test/java/com/primalimited/reliefshading/color/ColorPaletteTest.java
+++ src/test/java/com/primalimited/reliefshading/color/ColorPaletteTest.java
@@ -1,5 +1,6 @@
 package com.primalimited.reliefshading.color;
 
+import com.primalimited.reliefshading.number.Invalid;
 import org.junit.jupiter.api.Test;
 
 import java.awt.*;
@@ -11,12 +12,41 @@ class ColorPaletteTest {
     public void invalidNColors() {
         assertThrows(IllegalArgumentException.class,
                 () -> ColorPalette.create(0));
+
+        assertThrows(IllegalArgumentException.class,
+                () -> ColorPalette.create(Invalid.INVALID_INT));
     }
 
     @Test
     public void emptyControlPoints() {
         assertThrows(IllegalStateException.class,
                 () -> ColorPalette.create(256));
+    }
+
+    @Test
+    public void invalidControlPoints_noZeroIndex() {
+        // a valid color palette must specify control points
+        // on both ends
+
+        int nColors = 10;
+        // missing control point at index 0
+        ControlPoint cp0 = new ControlPoint(3, Color.BLACK.getRGB());
+        ControlPoint cp1 = new ControlPoint(9, Color.BLACK.getRGB());
+        assertThrows(IllegalStateException.class,
+                () -> ColorPalette.create(nColors, cp0, cp1));
+    }
+
+    @Test
+    public void invalidControlPoints_noEndIndex() {
+        // a valid color palette must specify control points
+        // on both ends
+
+        int nColors = 10;
+        // missing control point at index 9 (nColors-1)
+        ControlPoint cp0 = new ControlPoint(0, Color.BLACK.getRGB());
+        ControlPoint cp1 = new ControlPoint(5, Color.BLACK.getRGB());
+        assertThrows(IllegalStateException.class,
+                () -> ColorPalette.create(nColors, cp0, cp1));
     }
 
     @Test
@@ -33,5 +63,16 @@ class ColorPaletteTest {
         ColorPalette solidPalette = ColorPalette.solid(rgb);
         assertEquals(1, solidPalette.nColors());
         assertEquals(rgb, solidPalette.rgb(0));
+    }
+
+    @Test
+    public void badIndexArgForRGBQuery() {
+        int rgb = Color.ORANGE.getRGB();
+        ColorPalette solidPalette = ColorPalette.solid(rgb);
+
+        assertAll("Bad index arg for rgb()",
+                () -> assertThrows(IllegalArgumentException.class, () -> solidPalette.rgb(-1)),
+                () -> assertThrows(IllegalArgumentException.class, () -> solidPalette.rgb(2))
+        );
     }
 }
--- src/test/java/com/primalimited/reliefshading/color/ControlPointTest.java
+++ src/test/java/com/primalimited/reliefshading/color/ControlPointTest.java
@@ -25,10 +25,17 @@ class ControlPointTest {
     @Test
     public void testEquals() {
         ControlPoint controlPoint0 = new ControlPoint(0, 0);
+        assertEquals(controlPoint0, controlPoint0);
+
         ControlPoint controlPoint1 = new ControlPoint(0, 0);
         assertEquals(controlPoint0, controlPoint1);
         assertTrue(controlPoint0.equals(controlPoint1));
         assertTrue(controlPoint1.equals(controlPoint0));
+
+        // verify prechecks in equals()
+        ControlPoint nullPoint = null;
+        assertFalse(controlPoint0.equals(nullPoint));
+        assertFalse(controlPoint0.equals(Integer.valueOf(3)));
     }
 
     @Test
@@ -47,6 +54,29 @@ class ControlPointTest {
         assertNotEquals(controlPoint0, controlPoint1);
         assertFalse(controlPoint0.equals(controlPoint1));
         assertFalse(controlPoint1.equals(controlPoint0));
+    }
+
+    @Test
+    public void testHashCode() {
+        // same
+        ControlPoint controlPoint0 = new ControlPoint(0, 0);
+        ControlPoint controlPoint1 = new ControlPoint(0, 0);
+        assertEquals(controlPoint0.hashCode(), controlPoint1.hashCode());
+
+        // different index
+        ControlPoint cp0 = new ControlPoint(0, 0);
+        ControlPoint cp1 = new ControlPoint(1, 0);
+        assertNotEquals(cp0.hashCode(), cp1.hashCode());
+
+        // different color
+        ControlPoint c0 = new ControlPoint(0, Color.GRAY.getRGB());
+        ControlPoint c1 = new ControlPoint(0, Color.YELLOW.getRGB());
+        assertNotEquals(c0.hashCode(), c1.hashCode());
+
+        // different index and color
+        ControlPoint p0 = new ControlPoint(0, Color.GRAY.getRGB());
+        ControlPoint p1 = new ControlPoint(7, Color.YELLOW.getRGB());
+        assertNotEquals(p0.hashCode(), p1.hashCode());
     }
 
     @Test
