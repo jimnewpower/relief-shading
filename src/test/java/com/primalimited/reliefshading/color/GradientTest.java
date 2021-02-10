5f5380abebe4f7c0c01595640948130f0a0701f8
Jim Newpower <jimnewpower@gmail.com>
2021-02-10 15:30:58 -0700

    Test gradient

--- src/test/java/com/primalimited/reliefshading/color/GradientTest.java
+++ src/test/java/com/primalimited/reliefshading/color/GradientTest.java
@@ -11,5 +11,47 @@ class GradientTest {
     public void testGradient() {
         assertEquals(Color.WHITE.getRGB(), Gradient.computeRGB(0.f, Color.WHITE, Color.BLACK));
         assertEquals(Color.BLACK.getRGB(), Gradient.computeRGB(1.f, Color.WHITE, Color.BLACK));
+
+        assertEquals(
+                new Color(128, 128, 128).getRGB(),
+                Gradient.computeRGB(0.5f, Color.WHITE, Color.BLACK)
+        );
+        assertEquals(
+                new Color(128, 128, 128).getRGB(),
+                Gradient.computeRGB(0.5f, Color.BLACK, Color.WHITE)
+        );
+    }
+
+    @Test
+    public void invalidFraction() {
+        Color begin = Color.BLACK;
+        Color end = Color.WHITE;
+
+        assertAll(
+                "Invalid fraction argument",
+                () -> assertThrows(
+                        IllegalArgumentException.class,
+                        () -> Gradient.computeRGB(-0.01f, begin, end)
+                ),
+                () -> assertThrows(
+                        IllegalArgumentException.class,
+                        () -> Gradient.computeRGB(1.01f, begin, end)
+                )
+        );
+    }
+
+    @Test
+    public void nullColorArgs() {
+        assertAll(
+                "Invalid color arguments",
+                () -> assertThrows(
+                        NullPointerException.class,
+                        () -> Gradient.computeRGB(0.5f, null, Color.ORANGE)
+                ),
+                () -> assertThrows(
+                        NullPointerException.class,
+                        () -> Gradient.computeRGB(0.5f, Color.GRAY, null)
+                )
+        );
     }
 }
