ff0f379f23b0ef3b660b87584e283bcda4305361
Jim Newpower <jimnewpower@gmail.com>
2021-02-10 14:05:13 -0700

    Add javadoc class comment

--- src/main/java/com/primalimited/reliefshading/algorithm/ReliefShader.java
+++ src/main/java/com/primalimited/reliefshading/algorithm/ReliefShader.java
@@ -3,8 +3,11 @@ package com.primalimited.reliefshading.algorithm;
 import com.primalimited.reliefshading.grid.Grid;
 import com.primalimited.reliefshading.prefs.Preferences;
 
+/**
+ * Interface for relief shading algorithms.
+ */
 public interface ReliefShader {
-    public static ReliefShader create(Preferences preferences) {
+    static ReliefShader create(Preferences preferences) {
         return ReliefShaderImpl.create(preferences);
     }
 
