742af79ee71579136db15c147bd601489ef8ced5
Jim Newpower <jimnewpower@gmail.com>
2021-02-11 12:39:12 -0700

    Add javadoc

--- src/main/java/com/primalimited/reliefshading/algorithm/ZFactorCartesian.java
+++ src/main/java/com/primalimited/reliefshading/algorithm/ZFactorCartesian.java
@@ -2,9 +2,18 @@ package com.primalimited.reliefshading.algorithm;
 
 import com.primalimited.reliefshading.units.Length;
 
+/**
+ * Compute z-factor for Cartesian coordinates.
+ */
 public class ZFactorCartesian implements ZFactorDem {
     private final float zFactor;
 
+    /**
+     * Constructor
+     *
+     * @param xyUnit unit for x,y values
+     * @param zUnit unit for z values
+     */
     public ZFactorCartesian(Length xyUnit, Length zUnit) {
         this.zFactor = (float) (zUnit.metersPerUnit / xyUnit.metersPerUnit);
     }
