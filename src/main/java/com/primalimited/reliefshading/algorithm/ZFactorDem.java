b7efc8f5bccd8eeb9ca7f2ee6940598668a164a5
Jim Newpower <jimnewpower@gmail.com>
2021-02-11 12:33:22 -0700

    Clarify javadoc comment

--- src/main/java/com/primalimited/reliefshading/algorithm/ZFactorDem.java
+++ src/main/java/com/primalimited/reliefshading/algorithm/ZFactorDem.java
@@ -2,9 +2,9 @@ package com.primalimited.reliefshading.algorithm;
 
 public interface ZFactorDem {
     /**
-     * Return the number of ground (x,y) units in one surface z-unit.
+     * Return the number of ground (x,y) units in one surface elevation unit.
      *
-     * @return the number of ground (x,y) units in one surface z-unit.
+     * @return the number of ground (x,y) units in one surface elevation unit.
      */
     default float getZFactor() {
         return 1.0f;
